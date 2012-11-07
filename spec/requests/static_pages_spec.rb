455e9e49f3e266280ffbf8ce6fe09a3b6606e864
Javier Pelado <javier.pelado@gmail.com>
2012-11-07 18:03:59 +0100

    Completado el formulario de signup erroneo

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -63,4 +63,20 @@ describe "Static pages" do
     click_link "sample app"
     page.should have_selector 'title', text: full_title('')
   end
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_selector('h1',    text: 'Sign up') }
+    it { should have_selector('title', text: 'Sign up') }
+  end
+
+  describe "profile page" do
+    # Code to make a user variable
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
 end
