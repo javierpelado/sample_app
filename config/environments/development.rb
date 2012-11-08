55551b5ef98294a5cc6b096ee915144303f4b8b7
Javier Pelado <javier.pelado@gmail.com>
2012-11-08 11:28:56 +0100

    Finalizados los ejercicios del capitulo 7

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,8 @@ SampleApp::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.i18n.available_locales = :es
+  
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
