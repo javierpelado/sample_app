f3cc68181a92536690f4258dbca0b0c8046efc61
Javier Pelado <javier.pelado@gmail.com>
2012-11-08 15:51:57 +0100

    Solucionado error de login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,6 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		render 'new'	
-	end
-
-	def create
 		user = User.find_by_email(params[:session][:email].downcase)
   		if user && user.authenticate(params[:session][:password])
     		# Sign the user in and redirect to the user's show page.
